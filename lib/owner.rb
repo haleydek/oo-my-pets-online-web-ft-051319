b75f46035654a0babbb1bde498118dadb314cc67
Haley DeKeyser <hcd09@live.com>
2019-05-29 14:24:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -65,7 +65,11 @@ class Owner
   end
   
   def list_pets
+    fish_count = @pets[:fishes].size
+    dog_count = @pets[:dogs].size
+    cat_count = @pets[:cats].size
     
+    puts "I have #{fish_count} fish, #{dog_count} dog(s), and #{cat_count} cat(s)."
   end
   
 end
