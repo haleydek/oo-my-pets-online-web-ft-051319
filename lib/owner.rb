19a398557c60bb53833234cec64d63767ac3aee8
Haley DeKeyser <hcd09@live.com>
2019-05-29 13:46:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,15 @@
 class Owner
+  attr_accessor :pets
+  attr_reader :name
+  @@all = []
+  counter = 0
   
+  def initialize
+    @pets = {}
+  end
   
+  def name(name)
+    @name = name
+  end
   
 end
