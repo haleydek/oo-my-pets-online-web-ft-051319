2f4da106a34a7d7407b2e790717e2870666e18b6
Haley DeKeyser <hcd09@live.com>
2019-05-29 14:18:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,7 +6,7 @@ class Owner
   @@all = []
   counter = 0
   
-  def initialize(pets)
+  def initialize
     @pets = { fishes: [], cats: [], dogs: [] }
     @species = "human"
     self.class.all << self
